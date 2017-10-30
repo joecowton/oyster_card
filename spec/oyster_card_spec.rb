524bccc101ee5e2f71a993d065473cb4f12dd9ff
Joe Cowton <joecowton@gmail.com>
2017-10-30 15:01:08 +0000

    added top_up method

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,7 +6,20 @@ describe Oystercard do
     it "should be zero" do
       expect(subject.balance).to eq(0)
     end
-
   end
+
+  describe  "#top_up" do
+    it "should take an arguement and add to the balance" do
+        expect(subject).to respond_to(:top_up).with(1).argument
+      end
+    end
+
+    it "should add 10 to balance" do
+      subject.top_up(10)
+      expect(subject.balance).to eq(10)
+    end
+
+
+
 
 end
